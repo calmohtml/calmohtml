840b5404b6ab252c55d77ab336d16816653340cc
calmohtml <contactosegovia@protonmail.com>
2021-09-08 15:49:33 -0300

    Templated ready

---
+++ pages/_document.jsx
@@ -0,0 +1,45 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // eslint-disable-next-line react/display-name
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>{/* Insert Google Fonts here if needed */}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 50px;
+`;
+
+const Home = () => {
+  return <Title>My page</Title>;
+};
+
+export default Home;
